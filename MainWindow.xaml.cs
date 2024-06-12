d0b1e81c948b767be55f1af322f666558d78ef9f
KiselyovSaint <116631267+KiselyovSaint@users.noreply.github.com>
2024-06-13 00:20:56 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestDemo.ClassHelper;
+using TestDemo.DB;
+
+namespace TestDemo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lvListUser.ItemsSource = EFClass.context.User.ToList();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                User user = button.DataContext as User;
+                if (user != null)
+                {
+                    EFClass.context.User.Remove(user);
+                    EFClass.context.SaveChanges();
+                    lvListUser.ItemsSource = ClassHelper.EFClass.context.User.ToList();
+                }
+            }
+        }
+    }
+}
---
+++ RegWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TestDemo.DB;
+
+namespace TestDemo.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegWindow.xaml
+    /// </summary>
+    public partial class RegWindow : Window
+    {
+        public RegWindow()
+        {
+            InitializeComponent();
+            cmbGender.ItemsSource = ClassHelper.EFClass.context.Gender.ToList();
+            cmbGender.DisplayMemberPath = "NamGender";
+            cmbGender.SelectedIndex = 0;
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+
+            user.FName = txbFName.Text;
+            user.LName = txbLName.Text;
+            user.Login = txbLogin.Text;
+            user.Password = pwbPassword.Password;
+            user.Birthday = dpBirthday.SelectedDate.Value;
+            user.IDGender = (cmbGender.SelectedItem as Gender).ID;
+
+            ClassHelper.EFClass.context.User.Add(user);
+            ClassHelper.EFClass.context.SaveChanges();
+
+            MessageBox.Show("Gotovo");
+        }
+    }
+}
